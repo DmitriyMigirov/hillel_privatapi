d279ff0af69a0a23080fe78ef2e98f9cbdcf33fa
Dmitriy <dmitriystels@gmail.com>
2022-09-29 16:39:10 +0300

    Update

---
+++ utils.py
@@ -0,0 +1,19 @@
+import requests
+
+def get_currency_exchange_rate( currency_a: str, currency_b: str, currency_date:str, curency_curse:str):
+
+    response = requests.get(f'https://api.privatbank.ua/p24api/exchange_rates?json&date={currency_date}')
+    json = response.json()
+
+    if response.status_code == 200:
+        for i in range(len(json.get("exchangeRate"))):
+            if json.get("exchangeRate")[i].get('baseCurrency') == currency_a and json.get("exchangeRate")[i].get('currency')  == currency_b:
+                rate_buyNB = json.get("exchangeRate")[i].get('saleRateNB')
+                rate_sellNB = json.get("exchangeRate")[i].get('purchaseRateNB')
+                rate_buy = json.get("exchangeRate")[i].get('saleRate')
+                rate_sell = json.get("exchangeRate")[i].get('purchaseRate')
+                if curency_curse == 'НБУ':
+                    return f'Курс НБУ на {currency_date} у випадку конвертації {currency_a} в {currency_b} - курс {rate_buyNB} : {rate_sellNB}'
+                else:
+                    return f'Курс Приватбанку на {currency_date} у випадку конвертації {currency_a} в {currency_b} - для покупки {rate_buy} та для продажу {rate_sell}'
+    return f"Помилка api {response.status_code}: {json.get('errorDescription')}"
